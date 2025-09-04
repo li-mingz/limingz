156ff1d109ee552957f4d4fd9b1ca5ee8d3d4f8d
17330915383 <lm17330915383@163.com>
2025-09-05 00:55:56 +0800

    补全要替换的方块

--- src/main/java/com/limingz/mymod/util/BlockReplaceList.java
+++ src/main/java/com/limingz/mymod/util/BlockReplaceList.java
@@ -1,28 +1,32 @@
 package com.limingz.mymod.util;
 
+import net.mcreator.caerulaarbor.CaerulaArborMod;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 
 public class BlockReplaceList {
     // 创建替换映射
     public static Map<String, String> replace_name_Map = new HashMap<>();
     public static Map<String, ResourceLocation> replace_name_ResourceLocation_Map = new HashMap<>();
     public static Map<String, List<String>> replace_properties_Map = new HashMap();
-    public static Predicate<BlockState> blockStatePredicate = new Predicate<BlockState>() {
-        @Override
-        public boolean test(BlockState blockState) {
-            return false;
-        }
-    };
     static {
-        replace_name_Map.put("minecraft:deepslate", "minecraft:oak_log");
-        replace_properties_Map.put("minecraft:deepslate", Arrays.asList("axis"));  // 继承朝向
+        replace_name_Map.put(CaerulaArborMod.MODID+":sea_trail_init", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":sea_trail_growing", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":sea_trail_grown", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":ocean_ovary", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":red_ovary", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":trail_mushroom", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":sea_trail_stop", "minecraft:air");
+        replace_name_Map.put(CaerulaArborMod.MODID+":sea_trail_solid", "minecraft:grass_block");
+        replace_name_Map.put(CaerulaArborMod.MODID+":trail_pulse", "minecraft:grass_block");
+        replace_name_Map.put(CaerulaArborMod.MODID+":trail_log", "minecraft:oak_log");
+        replace_properties_Map.put(CaerulaArborMod.MODID+":trail_log", Arrays.asList("axis"));  // 继承朝向
+        replace_name_Map.put(CaerulaArborMod.MODID+":trail_leave", "minecraft:oak_leaves");
+        replace_properties_Map.put(CaerulaArborMod.MODID+":trail_leave", Arrays.asList("waterlogged"));  // 继承含水状态
     }
     static {
         replace_name_Map.forEach((name1, name2) -> {
