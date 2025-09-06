d4dc7fbae94e8e25ff3ffcc7b19bc024ff9db6b9
17330915383 <lm17330915383@163.com>
2025-09-06 11:47:33 +0800

    添加tag，修改物品名称

--- src/main/java/com/limingz/mymod/loot/CropsModifier.java
+++ src/main/java/com/limingz/mymod/loot/CropsModifier.java
@@ -1,9 +1,13 @@
 package com.limingz.mymod.loot;
 
-import com.limingz.mymod.datagen.GlobalLootModifier;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.CropBlock;
@@ -17,6 +21,8 @@ import net.minecraftforge.common.loot.LootModifier;
 import org.jetbrains.annotations.NotNull;
 
 public class CropsModifier extends LootModifier {
+    // 富营养的标签
+    public static final String TAG_NUTRITIOUS = "mymod:nutritious_food";
     // 定义 Codec 用于序列化
     public static final Codec<CropsModifier> CODEC = RecordCodecBuilder.create(inst ->
             LootModifier.codecStart(inst).apply(inst, CropsModifier::new)
@@ -37,16 +43,26 @@ public class CropsModifier extends LootModifier {
         for(ItemStack itemStack : generatedLoot){
             // 获取物品的食物属性
             FoodProperties foodProperties = itemStack.getItem().getFoodProperties(itemStack, null);
-            // 非 null 表示可食用
-            if(foodProperties != null){
-                foodProperties.getNutrition(); // 饱食度
-                foodProperties.getSaturationModifier(); // 饱和度
-            }
+            // 跳过不可食用的和已经有标签的
+            if(foodProperties == null || isNutritious(itemStack)) continue;
+            CompoundTag tag = itemStack.getOrCreateTag();
+            tag.putFloat(TAG_NUTRITIOUS, 1.0f);
+            // 更改物品名称
+            MutableComponent oldName = itemStack.getHoverName().copy();
+            Style oldStyle = oldName.getStyle();
+            if(oldStyle.isEmpty()) oldStyle = oldStyle.applyFormats(ChatFormatting.WHITE);
+            MutableComponent newName = Component.literal("富营养的 ").withStyle(ChatFormatting.GREEN)
+                    .append(oldName.withStyle(oldStyle));
+            itemStack.setHoverName(newName);
         }
         // 计算区块坐标
         int chunkX = ((int) Math.floor(originVector.x)) >> 4;
         int chunkZ = ((int) Math.floor(originVector.z)) >> 4;
         return generatedLoot;
+    }
+
+    private boolean isNutritious(ItemStack stack) {
+        return stack.hasTag() && stack.getTag().contains(TAG_NUTRITIOUS);
     }
 
     @Override
