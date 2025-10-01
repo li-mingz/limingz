ce70cad3534bb238804569e7667a34e7f1b953f8
17330915383 <lm17330915383@163.com>
2025-10-01 22:34:52 +0800

    尝试使用svg播放动画

--- src/main/java/com/limingz/mymod/block/entity/client/DeepBlueLabAccessControlDoorRenderer.java
+++ src/main/java/com/limingz/mymod/block/entity/client/DeepBlueLabAccessControlDoorRenderer.java
@@ -1,20 +1,35 @@
 package com.limingz.mymod.block.entity.client;
 
 import com.limingz.mymod.block.entity.DeepBlueLabAccessControlDoorEntity;
+import com.limingz.mymod.gui.holographic_ui.config.UIConfig;
+import com.limingz.mymod.gui.holographic_ui.renderer.ui.other.DemoScreen;
+import com.limingz.mymod.gui.holographic_ui.renderer.ui.system.Svg;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class DeepBlueLabAccessControlDoorRenderer extends GeoBlockRenderer<DeepBlueLabAccessControlDoorEntity> {
-
+    private Svg svg;
     public DeepBlueLabAccessControlDoorRenderer(BlockEntityRendererProvider.Context context) {
         super(new DeepBlueLabAccessControlDoorModel());
+        svg = new Svg("Svg", 0, 0, 1920, 1080);
     }
+
     @Override
     public RenderType getRenderType(DeepBlueLabAccessControlDoorEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
         return RenderType.entityTranslucent(getTextureLocation(animatable));
+    }
+
+    @Override
+    public void actuallyRender(PoseStack poseStack, DeepBlueLabAccessControlDoorEntity animatable, BakedGeoModel model, RenderType renderType, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
+        svg.renderAll(bufferSource, poseStack, packedOverlay, animatable);;
     }
 }
---
+++ src/main/java/com/limingz/mymod/util/batik/SVGTextureManager.java
@@ -0,0 +1,90 @@
+package com.limingz.mymod.util.batik;
+
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.ImageTranscoder;
+import com.mojang.blaze3d.platform.NativeImage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.Resource;
+import net.minecraft.server.packs.resources.ResourceManager;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class SVGTextureManager {
+    private static final Map<ResourceLocation, ResourceLocation> TEXTURE_CACHE = new HashMap<>();
+
+    public static ResourceLocation getOrCreateTexture(ResourceLocation svgLocation, int width, int height) {
+        if (TEXTURE_CACHE.containsKey(svgLocation)) {
+            return TEXTURE_CACHE.get(svgLocation);
+        }
+
+        try {
+            ResourceManager resourceManager = Minecraft.getInstance().getResourceManager();
+            Optional<Resource> optionalResource = resourceManager.getResource(svgLocation);
+            Resource svgResource = optionalResource.orElseThrow(() ->
+                    new RuntimeException("SVG resource not found: " + svgLocation)
+            );
+
+            // 修正：使用 open() 方法获取输入流（需处理 IOException）
+            try (InputStream inputStream = svgResource.open()) {
+                BufferedImage bufferedImage = parseSVG(inputStream, width, height);
+
+                // 转换为 NativeImage
+                NativeImage nativeImage = new NativeImage(width, height, true);
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        nativeImage.setPixelRGBA(x, y, bufferedImage.getRGB(x, y));
+                    }
+                }
+
+                // 创建并注册动态纹理
+                DynamicTexture dynamicTexture = new DynamicTexture(nativeImage);
+                ResourceLocation textureId = Minecraft.getInstance().getTextureManager()
+                        .register("svg/" + svgLocation.getPath(), dynamicTexture);
+
+                TEXTURE_CACHE.put(svgLocation, textureId);
+                return textureId;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to open SVG input stream: " + svgLocation, e);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load SVG texture: " + svgLocation, e);
+        }
+    }
+
+    private static BufferedImage parseSVG(InputStream inputStream, int width, int height) throws Exception {
+        class CustomTranscoder extends ImageTranscoder {
+            private BufferedImage image;
+
+            @Override
+            public BufferedImage createImage(int w, int h) {
+                return new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            }
+
+            @Override
+            public void writeImage(BufferedImage img, TranscoderOutput out) {
+                this.image = img;
+            }
+
+            public BufferedImage getImage() {
+                return image;
+            }
+        }
+
+        CustomTranscoder transcoder = new CustomTranscoder();
+        transcoder.addTranscodingHint(ImageTranscoder.KEY_WIDTH, (float) width);
+        transcoder.addTranscodingHint(ImageTranscoder.KEY_HEIGHT, (float) height);
+        transcoder.transcode(new TranscoderInput(inputStream), null);
+        return transcoder.getImage();
+    }
+
+    public static void clearCache() {
+        TEXTURE_CACHE.clear();
+    }
+}
